66507244a3ec4e61ce490e9341cfe1fbcaa4a5e5
Salva098 <Salvadorgonzalezluque1@gmail.com>
2022-03-07 13:59:54 +0100

    Update stripe.service.ts

--- src/app/_servicies/stripe.service.ts
+++ src/app/_servicies/stripe.service.ts
@@ -13,13 +13,13 @@ export class StripeService {
   constructor(private http : HttpClient) {}
 
   existPremium(){
-    return this.http.get(this.urlapi+"Stripe/Exist",
+    return this.http.get(this.urlapi+"Contrato/Exist",
     {responseType: 'text'}
     )
   }
 
   generarUrlPago(empresa:empresa){
-    return this.http.post(this.urlapi+"Stripe",empresa,
+    return this.http.post(this.urlapi+"Contrato",empresa,
     {responseType: 'text'}
     )
   }
